8476c22784b77321b2e71b325345296192d8d179
notogawa <n.ohkawa@gmail.com>
2013-07-19 22:40:00 +0900

    forget to add.

---
+++ test/double.hs
@@ -0,0 +1,41 @@
+import Network.Socket ( send, recv )
+import Control.Concurrent
+import Control.Monad
+import System.Exit
+import System.Posix.Directory
+import System.Posix.Files
+import System.Posix.IO
+import System.Posix.Process
+import System.Posix.Signals
+
+import System.Posix.Graceful
+
+main :: IO ()
+main = daemonize $ graceful settings
+    where
+      settings = GracefulSettings
+                 { gracefulSettingsPortNumber = 8080
+                 , gracefulSettingsWorkerCount = 4
+                 , gracefulSettingsInitialize = return ()
+                 , gracefulSettingsApplication = application
+                 , gracefulSettingsFinalize = const $ return ()
+                 , gracefulSettingsSockFile = "/tmp/echo-server.sock"
+                 , gracefulSettingsPidFile = "/tmp/echo-server.pid"
+                 , gracefulSettingsBinary = "/tmp/echo-server"
+                 }
+      application sock _ = forever $ recv sock 1024 >>= send sock . (\x -> x ++ x)
+
+daemonize :: IO () -> IO ()
+daemonize application = do
+  void $ setFileCreationMask 0
+  void $ forkProcess $ do
+    void createSession
+    void $ forkProcess $ do
+      changeWorkingDirectory "/"
+      devnull <- openFd "/dev/null" ReadWrite Nothing defaultFileFlags
+      let sendTo fd' fd = closeFd fd >> dupTo fd' fd
+      mapM_ (sendTo devnull) [ stdInput, stdOutput, stdError ]
+      void $ installHandler sigHUP Ignore Nothing
+      application
+    exitImmediately ExitSuccess
+  exitImmediately ExitSuccess
